bcb71e3562997ee582fd9a3b9e1e34f4aecd3955
Zak Goneau <zak.goneau@stu.ocsb.ca>
2025-02-05 15:31:09 +0000

    Program prints messages in Java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// This program greets Ms Raffin and asks a question
+
+// Creating class
+class HelloWorld {
+    public static void main(String[] args) {
+    
+        // Print "Hello, Ms Raffin"
+        System.out.println("Hello, Ms Raffin");
+        
+        // Print a customized message
+        System.out.println("How was your day?");
+        
+    }
+}
